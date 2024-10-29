88b58edf0a51a86d855900fb3e8ace3d3274d584
Mr-Moose-dev <filip.lorin@gmail.com>
2024-10-29 01:18:00 +0100

    not quite there but i'm going to sleep

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,26 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+  public:
+    ResourceManager() : data{new Resource()} {}  // konstruktor domyślny
+    ResourceManager(const ResourceManager &rm) : data{rm.data} {}   // konstruktor kopiujący
+    ResourceManager& operator=(const ResourceManager &rm) // kopiujący operator przypisania
+    {
+      if(this == &rm) {return *this;}
+      delete data;
+      data = NULL;
+      data = rm.data;
+      return *this;
+    }
+    ResourceManager(ResourceManager&& rm) //konstruktor przenoszący
+    {
+
+    }
+    ResourceManager& operator=(ResourceManager&& rm) //przenoszący operator przypisania
+    {
+      return *this;
+    }
+    ~ResourceManager() { delete data; }   //destruktor
+    const double get() { return data->get(); }
+    Resource* data;
 };
