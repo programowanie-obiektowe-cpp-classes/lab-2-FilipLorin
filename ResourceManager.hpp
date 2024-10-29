2271d1b532080edc0c7eb49210c5faf54de4e6af
Mr-Moose-dev <filip.lorin@gmail.com>
2024-10-29 11:04:34 +0100

    solved

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -6,13 +6,17 @@ class ResourceManager
 {
   public:
     ResourceManager() : data{new Resource()} {}  // konstruktor domyślny
-    ResourceManager(const ResourceManager &rm) : data{rm.data} {}   // konstruktor kopiujący
+    ResourceManager(const ResourceManager &rm)   // konstruktor kopiujący
+    {
+      data = new Resource();
+      *data = *(rm.data);
+    }
     ResourceManager& operator=(const ResourceManager &rm) // kopiujący operator przypisania
     {
-      if(this == &rm) {return *this;}
       delete data;
       data = NULL;
-      data = rm.data;
+      data = new Resource();
+      *data = *(rm.data);
       return *this;
     }
     ResourceManager(ResourceManager&& rm) //konstruktor przenoszący
